8cf42d6485484ea5b7f4b7de8a1cbe54afabdb3f
Curtis Olinger <colinger1@rh.com>
2025-05-17 16:21:18 -0700

    Initial commit

---
+++ ridge.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+
+rng = np.random.default_rng()
+rng.integers(low=1, high=20, size=4)
+
+data = {'11am': ,
+        '12pm': ,
+        '1pm': ,
+        '2pm': ,
+        '3pm': [100, 101, 150, 89],
+        'flash_demand': [200, 250, 300, 120]
+}
+
+X = np.array(data['3pm']).reshape(4,1)
+y = np.array(data['flash_demand'])
+
+reg = LinearRegression().fit(X, y)
+
+df = pd.DataFrame(data)
+# df.plot.scatter(x='3pm', y='flash_demand')
+
+# Generate predicted values for plotting the best-fit line
+x_fit = np.linspace(np.array(data['3pm']).min(), np.array(data['3pm']).max(), 100).reshape(-1, 1) # Generate evenly spaced x values
+y_fit = reg.predict(x_fit) # Compute corresponding y values
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.scatter(data['3pm'], data['flash_demand'])
+ax.plot(x_fit, y_fit, color='red', label='Least Squares Fit') # Add regression line
+ax.set_xlabel('Demand at 3pm')
+ax.set_ylabel('flash_demand')
+ax.set_title('Plot of demand at 3pm vs EOD flash_demand')
+ax.legend()
+plt.show()
+
+
+
+
+
+
+# print(reg.coef_)
+
+# print(X)
+
+# X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+# y = 1 * x_0 + 2 * x_1 + 3
+# y = np.dot(X, np.array([1, 2])) + 3
+
+# print(X.shape)
+
+
+observation_1  = np.array([10, 20, 22, 5])
+observation_2 = np.array([6, 7, 8, 2])
+observation_3 = np.array([11, 5, 1, 10])
+
+df = pd.DataFrame([
+    observation_1,
+    observation_2], 
+    columns=['A', 'B', 'C', 'D']
+)
+
+# print(df)
+
